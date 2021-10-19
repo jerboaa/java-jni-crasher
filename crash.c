c0c0ab94e8b28abe0e0e025e239c36cb52521f0c
Severin Gehwolf <sgehwolf@redhat.com>
2021-10-19 10:48:03 +0200

    Add parameter names in JNI

--- crash.c
+++ crash.c
@@ -1,6 +1,6 @@
 #include "CrashJNI.h"
 
-JNIEXPORT jint JNICALL Java_CrashJNI_doIt(JNIEnv *, jobject) {
+JNIEXPORT jint JNICALL Java_CrashJNI_doIt(JNIEnv* env, jobject obj) {
       int* j = NULL;
       jint a = (jint)*j; // NULL* deref
       return a;
